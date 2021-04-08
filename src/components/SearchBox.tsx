739ff8bc42f161cf4eddd0f721eed2190c2f13fc
Andrei Dragu <andrei.dragu@123formbuilder.com>
2021-04-08 22:50:17 +0300

    Refactor SearchBox component to use a controlled input

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,18 +1,52 @@
 import type React from 'react';
-import { Input } from '@chakra-ui/react';
+import { useState, useRef } from 'react';
+import { Input, InputGroup, InputRightElement, IconButton } from '@chakra-ui/react';
+import { FiX } from 'react-icons/fi';
 
 type TSearchBoxProps = {
     onSearchChange: (value: string) => void;
 };
 
 const SearchBox: React.FC<TSearchBoxProps> = ({ onSearchChange }) => {
+    const [value, setValue] = useState<string>('');
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleClearClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+        if (value) {
+            onSearchChange('');
+            setValue('');
+            inputRef.current?.focus();
+        }
+    };
 
     const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
-        onSearchChange(event.target.value);
+        const value = event.target.value;
+        onSearchChange(value);
+        setValue(value);
     };
 
     return (
-        <Input m={3} bg="blue.50" placeholder="Search a robot" maxW="250px" onChange={handleInputChange} />
+        <InputGroup m={3} bg="blue.50" maxW="250px" mx="auto">
+            {value &&
+                <InputRightElement
+                    children={
+                        <IconButton
+                            variant="ghost"
+                            size="sm"
+                            icon={<FiX />}
+                            aria-label="Clear search field"
+                            _focus={{ boxShadow: 'none' }}
+                            onClick={handleClearClick} />
+                    }
+                />
+            }
+            <Input
+                placeholder="Search a robot"
+                ref={inputRef}
+                value={value}
+                onChange={handleInputChange}
+            />
+        </InputGroup>
     );
 };
 
